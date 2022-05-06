166577cda8567169632111accd95eef9701dd170
soahg <mislamsohag@gmail.com>
2022-05-06 20:03:48 +0600

    page upload done

--- src/Componants/OurProduts/OurProducts.js
+++ src/Componants/OurProduts/OurProducts.js
@@ -1,21 +1,31 @@
 import Navber from '../../Shared/Header/Navber/Navber';
 import { Link, NavLink } from 'react-router-dom';
-import useProducts from '../../Hooks/useProducts';
-import { useState } from 'react';
+// import useProducts from '../../Hooks/useProducts';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const OurProducts = () => {
-    const [products] = useProducts();
+    const [products, setProducts] = useState([]);
+    const [limit, setLimit] = useState(6);
+    const [pageNumber, setPageNumber] = useState(0);
 
-    const [page, setpage] = useState(2);
+    useEffect(() => {
+        fetch(`http://localhost:5000/ourProducts?limit=${limit}&pageNumber=${pageNumber}`)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+
+    }, [limit, pageNumber]);
+
+
 
     return (
         <div>
             <Navber></Navber>
             <section className='container text-center'>
-                <h1>Our Products: {products.length}</h1>
+
                 <div className='row'>
                     {
-                        products?.map(product => (
+                        products?.length ? products?.map(product => (
                             <div className='col-sm-12 col-md-6 col-lg-4 g-3' key={product._id}>
                                 <NavLink className='nav-link' to="/pDetails">
                                     <div className="card" >
@@ -30,13 +40,20 @@ const OurProducts = () => {
                                     </div>
                                 </NavLink>
                             </div>
-                        ))
+                        )) : <div>No Product Found</div>
                     }
                 </div>
                 <div className='d-flex justify-content-end'>
                     {
-                        [...Array(page).keys()].map(number => <div className='mx-3 border border-dark px-3 py-1'>{number + 1}</div>)
+                        [...Array(5).keys()].map(number => <div key={number.toString()} onClick={() => setPageNumber(number)} className={`mx-3 border border-dark px-3 py-1 ${pageNumber === number ? "bg-black text-white" : ""} `}>{number + 1}</div>)
                     }
+                    <select onChange={(event) => setLimit(event.target.value)} >
+                        <option value="6">6</option>
+                        <option value="12">12</option>
+                        <option value="18">18</option>
+                        <option value="24">24</option>
+                        <option value="30">30</option>
+                    </select>
                 </div>
             </section>
         </div>
--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -1,15 +1,17 @@
 
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 const useProducts = () => {
 
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        // fetch('http://localhost:5000/ourProducts')
-        fetch('http://localhost:5000/ourProducts?limit=6')
-            .then(res => res.json())
-            .then(data => setProducts(data));
-    }, []);
-    return [products, setProducts];
+    // const [products, setProducts] = useState([]);
+    // const [limit, setLimit] = useState(5);
+
+    // useEffect(() => {
+    //     // fetch('http://localhost:5000/ourProducts')
+    //     fetch(`http://localhost:5000/ourProducts?limit=${limit}&pageNumber=1`)
+    //         .then(res => res.json())
+    //         .then(data => setProducts(data));
+    // }, [limit]);
+    // return [products, setProducts];
 }
 
 export default useProducts;
